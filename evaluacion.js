59964a2c678bbfe49d7921d2fd7401639a5b819a
Lucas Basterrica <basterricalucasdaniel@gmail.com>
2025-06-19 20:38:21 -0300

    descripcion de los cambios

---
+++ evaluacion.js
@@ -0,0 +1,12 @@
+//instrucciones para inicializar git y github
+// 1 crea repositorio en github
+// 2 inicializar el repo local en una carpeta de tu pc
+// : git init
+// 3 agrega el repo de github:
+// git remote add origin https://github.com/basterricalucas/evaluacionbasterricavivas.git 
+// 4 creamos un nuevo archivo y realizamos cambios
+// 5 agregamos los cambios al area de preparación
+//: git add
+// 6 realiza un commit de los cambios
+// : git commit -m "descripción de los cambios"
+// 7 sube los cambios a github: git push -u origin master
